ef8bc0298075aeaedb4a2dd7186ca10b3337eba0
bebofofum <bri.boehm@gmail.com>
2021-03-07 17:32:46 -0500

    commit tree 9948c55a458a72845f25d7732ee752567e3523d8

--- src/components/Band.js
+++ src/components/Band.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 
 class Band extends Component {
 
+  handleOnClick = () => {
+    console.log('band props', this.props)
+    this.props.deleteBand(this.props.id)
+  }
+
+  // bandList = () => props.bands.map((band,index) => <li key={index}>{band}</li>)
+
   render() {
+    console.log(this.props.band)
     return(
       <div>
-        Band Component
+        <li>{this.props.name}</li>
+        <button onClick={this.handleOnClick}>Delete</button>
       </div>
-    );
+    )
   }
 };
 
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -3,8 +3,22 @@ export default function manageBand(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
+      // creating a new variable object of the id and the name
+      const band = { 
+        id: Math.random(),
+        name: action.name }
 
-      return { ...state, bands: [...state.bands, action.name] }
+        // then return that state using a spread operator to carry all existing state elements and adding to the bands key an array of the new variable
+      return { ...state, 
+        bands: [...state.bands, band]}
+
+        case 'DELETE_BAND':
+          console.log("this is happening")
+          console.log(state.bands)
+          console.log('action', action)
+          const bands = state.bands.filter(band => band.id !== action.id);
+          console.log("returning", bands)
+          return { bands };
 
     default:
       return state;
